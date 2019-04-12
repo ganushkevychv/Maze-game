602b1856eea076791e8964a4065fd5fe7d5fbcc0
Vladyslav <ganushkevychv@gmail.com>
2019-04-12 18:13:58 +0200

    grid 9x9 created

--- js/Maze.js
+++ js/Maze.js
@@ -1,4 +1,5 @@
 "use strict"
+
 const maze = [
     '#########',
     '#.....#.#',
@@ -14,7 +15,7 @@ const maze = [
 const maze2 = [
     '#########',
     '#......T#',
-    '#.#######',
+    '#.###E###',
     '#.......#',
     '#######.#',
     '#.......#',
@@ -22,3 +23,74 @@ const maze2 = [
     '#......P#',
     '#########',
 ];
+const easterEgg = [
+    '#########',
+    '#.......#',
+    '#.#####.#',
+    '#...#T..#',
+    '###.#####',
+    '#...#...#',
+    '#.###.#.#',
+    '#.....#P#',
+    '#########',
+]
+const mazeBoard = document.createElement('div');
+
+document.body.appendChild(mazeBoard);
+
+maze.forEach(function(arrItem) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    let cells = arrItem.split('');
+
+    cells.forEach(function(cellItem) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        if (cellItem === '#') {
+            cell.classList.add('wall');
+        } else if (cellItem === '.') {
+            cell.classList.add('path');
+        } else if (cellItem === 'T') {
+            cell.classList.add('treasure');
+        } else if (cellItem === 'P') {
+            cell.classList.add('player');
+        } 
+        row.appendChild(cell)
+    });
+
+    mazeBoard.appendChild(row);
+});
+
+function nextMaze() {
+    mazeBoard.innerHTML = '';
+    maze2.forEach(function(arrItem) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+
+        let cells = arrItem.split('');
+
+        cells.forEach(function(cellItem) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            if (cellItem === '#') {
+                cell.classList.add('wall');
+            } else if (cellItem === '.') {
+                cell.classList.add('path');
+            } else if (cellItem === 'T') {
+                cell.classList.add('treasure');
+            } else if (cellItem === 'P') {
+                cell.classList.add('player');
+            } else if (cellItem === 'E') {
+                cell.classList.add('easterEgg')
+            }
+            row.appendChild(cell)
+        });
+
+        mazeBoard.appendChild(row);
+    });
+}
+
+function getIndex(target) {
+    return Array.from(target.parentElement.children).indexOf(target);
+};
